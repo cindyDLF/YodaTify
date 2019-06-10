b4157d8ba5be134b1d57da5ef14505ad74e31f0c
Cindy <32460435+cindyDLF@users.noreply.github.com>
2019-06-10 10:45:42 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,3 @@ export const config = {
   databaseURL: "https://yodatify.firebaseio.com/",
   authDomain: "localhost"
 };
-
-export const configApi = {
-  API_KEY: "605b5d07d4mshdc3646da210b51bp154162jsn6828e4df176b"
-};
