bddc0d98a5e3b697201625fceb6e23d52be7dab4
Cindy <32460435+cindyDLF@users.noreply.github.com>
2019-06-10 09:45:46 +0200

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-export const config = {
-  apiKey: "AIzaSyC94nzgiM5uoIZWe1J9L5_nX-DkC4rVPZk",
-  projectId: "yodatify",
-  databaseURL: "https://yodatify.firebaseio.com/",
-  authDomain: "localhost"
-};
